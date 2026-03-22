b253296d6a2cdbca808e9c3a1d4b9ac6428a8aa1
SomyaBaran <somyabaranwal6664@gmail.com>
2026-03-22 13:25:29 +0530

    added svgs.tsx

---
+++ frontend/src/assets/svgs.tsx
@@ -0,0 +1,27 @@
+import { FaFacebookF, FaGithub, FaGoogle, FaLinkedinIn } from "react-icons/fa";
+
+export const UserIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+        <path d="M12 12c2.7 0 4.8-2.1 4.8-4.8S14.7 2.4 12 2.4 7.2 4.5 7.2 7.2 9.3 12 12 12zm0 2.4c-3.2 0-9.6 1.6-9.6 4.8v2.4h19.2v-2.4c0-3.2-6.4-4.8-9.6-4.8z" />
+    </svg>
+);
+export const MailIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+        <path d="M20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4-8 5-8-5V6l8 5 8-5v2z" />
+    </svg>
+);
+export const LockIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+        <path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zm-6 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zm3.1-9H8.9V6c0-1.71 1.39-3.1 3.1-3.1 1.71 0 3.1 1.39 3.1 3.1v2z" />
+    </svg>
+);
+
+export const SocialRow = () => (
+    <div className="flex gap-[12px] justify-center">
+        {[<FaGoogle />, <FaFacebookF />, <FaGithub />, <FaLinkedinIn />].map((icon, i) => (
+            <div key={i} className="w-[40px] h-[40px] border border-[#ccc] flex items-center justify-center text-gray-700 text-[17px] rounded-[6px] hover:border-[#82A0F4] hover:text-[#82A0F4] cursor-pointer transition-all duration-200">
+                {icon}
+            </div>
+        ))}
+    </div>
+);
