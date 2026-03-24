ac338ac06d16d3df542bac67e8e70884fb4a5e6e
SomyaBaran <somyabaranwal6664@gmail.com>
2026-03-24 23:35:28 +0530

    added login.ts

--- frontend/src/api/login.ts
+++ frontend/src/api/login.ts
@@ -1,5 +1,12 @@
-const LoginUser = async (username: string, password: string) => {
-        
-};
+import { BACKEND_URL } from "../lib/env";
 
-export default LoginUser;
+export default async function LoginUser(email: string, password: string) {
+    const response = await fetch(`${BACKEND_URL}/auth/signin`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password })
+    });
+
+    const data = await response.json();
+    return data;
+}
