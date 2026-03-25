813413930bf6db21768000cc240d2c3ee8778658
SomyaBaran <somyabaranwal6664@gmail.com>
2026-03-25 20:12:54 +0530

    added custom hook

---
+++ frontend/src/hooks/useLoggedIn.ts
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { BACKEND_URL } from "../lib/env";
+
+export function useLoggedIn() {
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const checkAuth = async () => {
+            const token = localStorage.getItem("token");
+            if (!token) {
+                setLoggedIn(false);
+                setLoading(false);
+                return;
+            }
+
+            try {
+                const response = await fetch(`${BACKEND_URL}/check`, {
+                    method: "GET",
+                    headers: {
+                        "Authorization": token!,
+                        "Content-Type": "application/json"
+                    }
+                });
+
+                const data = await response.json();
+                if (data.error) {
+                    setLoggedIn(false);
+                } else {
+                    setLoggedIn(true);
+                }
+
+            } catch {
+                setLoggedIn(false);
+            }
+            setLoading(false);
+        }
+        checkAuth();
+    }, []);
+    return { loggedIn, loading }
+}
