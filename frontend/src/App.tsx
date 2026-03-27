251f4c993ea3e28c127c3957fc21375c4973fda0
SomyaBaran <somyabaranwal6664@gmail.com>
2026-03-28 00:28:09 +0530

    added new routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,17 +2,21 @@ import { Route, Routes } from "react-router-dom";
 import './App.css'
 import { Landing } from "./pages/Landing";
 import Auth from "./pages/Auth";
+import NewStory from "./pages/NewStory";
+import { Dashboard } from "./pages/Dashboard";
+import { Home } from "./pages/Home";
 
 function App() {
 
   return (
     <Routes>
-      <Route path="/auth" element={<Landing />} />
+      <Route path="/" element={<Landing />} />
       <Route path="/auth" element={<Auth />} />
+      <Route path="/new-story" element={<NewStory />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/home" element={<Home />} />
     </Routes>
   )
 }
 
 export default App
-
-
