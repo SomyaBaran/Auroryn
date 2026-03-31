431178f6400789026af8409a5f8e355a7c44309c
SomyaBaran <somyabaranwal6664@gmail.com>
2026-03-31 19:12:25 +0530

    added about section

---
+++ frontend/src/components/About.tsx
@@ -0,0 +1,18 @@
+import { motion } from "framer-motion";
+
+export function About() {
+    return (
+        <motion.div
+            initial={{ y: 60, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.9 }}
+            viewport={{ once: false, amount: 0.05 }}
+            className="relative z-10 min-h-screen bg-[#130409]"
+            style={{
+                borderRadius: "5px 5px 0 0",
+                marginTop: "-20px",
+            }}
+        >
+        </motion.div>
+    );
+}
