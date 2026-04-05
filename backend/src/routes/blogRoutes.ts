9ba83478988a1184a5eae96ad29774702beccaad
SomyaBaran <somyabaranwal6664@gmail.com>
2026-04-06 02:34:19 +0530

    blogRoutes

---
+++ backend/src/routes/blogRoutes.ts
@@ -0,0 +1,46 @@
+import { Router } from "express";
+import { type Request, type Response } from "express";
+import { middleware } from "../middlewares/middleware";
+import { prisma } from "../../lib/prisma";
+
+export const blogRouter = Router();
+blogRouter.post("/new", middleware, async (req: Request, res: Response) => {
+    const title = req.body.title;
+    const content = req.body.content;
+    //@ts-ignore
+    const authorId = req.userId;
+    if (!title || !content) {
+        return res.status(400).json({
+            error: "Title and description required"
+        });
+    }
+
+    const blog = await prisma.blog.create({
+        data: {
+            title,
+            content,
+            authorId
+        }
+    });
+
+    return res.status(201).json({
+        blog
+    });
+});
+
+blogRouter.get("/user-blogs", middleware, async (req: Request, res: Response) => {
+    //@ts-ignore
+    const authorId = req.userId;
+    if (!authorId) {
+        return res.status(400).json({
+            error: "Unauthorized"
+        });
+    }
+
+    const blogs = await prisma.blog.findMany({
+        where: { authorId }
+    });
+    return res.status(201).json({
+        blogs
+    });
+});
