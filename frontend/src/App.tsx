558f7df0fa771a419adf6b2e45baf260a32a086d
SomyaBaran <somyabaranwal6664@gmail.com>
2026-05-13 23:05:08 +0530

    init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import './App.css'
 import { Landing } from "./pages/Landing";
-import Auth from "./pages/Auth";
+// import Auth from "./pages/Auth";
 import NewStory from "./pages/NewStory";
 import { Dashboard } from "./pages/Dashboard";
 import { Home } from "./pages/Home";
@@ -11,7 +11,7 @@ function App() {
   return (
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/auth" element={<Auth />} />
+        {/* <Route path="/auth" element={<Auth />} /> */}
         <Route path="/new-story" element={<NewStory />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/home" element={<Home />} />
