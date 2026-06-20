eb9c41731907d27f61b6e43973328bffbf2f6564
SomyaBaran <somyabaranwal6664@gmail.com>
2026-06-20 21:09:39 +0530

    cors headers + point frontend to render

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,6 +13,7 @@ app.use(express.json());
 app.use(cors({
     origin: ["http://localhost:5173", "https://auroryn.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.use("/blog", blogRouter);
 
@@ -49,4 +50,3 @@ app.get("/check", middleware, async (req, res) => {
 app.listen(3000, () => {
     console.log("Port is listening on 3000");
 });
-process.stdin.resume();
